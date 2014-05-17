a13a9bf28c378cb2ec24fe408d5238e8258f1097
Maria Saveleva <nokiae72i@yandex.ru>
2014-05-17 17:30:45 +0400

    Added project files

---
+++ TimeKeeper/TKPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKPAppDelegate.h
+//  TimeKeeper
+//
+//  Created by Maria Saveleva on 17/05/14.
+//  Copyright (c) 2014 Maria Saveleva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TimeKeeper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimeKeeper
+//
+//  Created by Maria Saveleva on 17/05/14.
+//  Copyright (c) 2014 Maria Saveleva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TKPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TKPAppDelegate class]));
+    }
+}
