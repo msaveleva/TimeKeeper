49c5b40ffe6e9a4d9047d63e6197ae9989b398cf
Maria Saveleva <mariiasaveleva@gmail.com>
2014-06-02 15:02:52 +0400

    Added blur image for selecting time type mode

--- TimeKeeper/TKPEditViewController.m
+++ TimeKeeper/TKPEditViewController.m
@@ -11,18 +11,24 @@
 #import "TKPEditNameTableViewCell.h"
 #import "TKPCategoryTypeTableViewCell.h"
 #import "UIColor+CustomColors.h"
+#import "FXBlurView.h"
 
 static NSString * const kEditNameCell = @"editNameCell";
 static NSString * const kCategoryTypeCell = @"categoryTypeCell";
 
 static CGFloat const kEditNameCellHeight = 68.0f;
 static CGFloat const kCategoryTypeCellHeight = 63.0f;
+static CGFloat const kBlurRadius = 20.0f;
 
 @interface TKPEditViewController ()
 
 @property (weak, nonatomic) IBOutlet TKPHeaderView *headerView;
 @property (weak, nonatomic) IBOutlet UITableView *editTableView;
 @property (weak, nonatomic) IBOutlet UIButton *deleteButton;
+@property (strong, nonatomic) FXBlurView *blurView;
+@property (nonatomic) BOOL isSelectingTimeType;
+
+- (void)switchBlur;
 
 @end
 
@@ -40,6 +46,8 @@ static CGFloat const kCategoryTypeCellHeight = 63.0f;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.isSelectingTimeType = NO;
     
     //set header view mode
     [self.headerView enableEditionMode];
@@ -59,6 +67,12 @@ static CGFloat const kCategoryTypeCellHeight = 63.0f;
     [self.editTableView registerNib:editNameNib forCellReuseIdentifier:kEditNameCell];
     UINib *categoryTypeNib = [UINib nibWithNibName:@"TKPCategoryTypeTableViewCell" bundle:nil];
     [self.editTableView registerNib:categoryTypeNib forCellReuseIdentifier:kCategoryTypeCell];
+    
+    //make blur view
+    self.blurView = [[FXBlurView alloc] initWithFrame:self.view.frame];
+    self.blurView.backgroundColor = [UIColor clearColor];
+    self.blurView.tintColor = [UIColor clearColor];
+    self.blurView.blurRadius = kBlurRadius;
 }
 
 - (void)didReceiveMemoryWarning
@@ -68,6 +82,15 @@ static CGFloat const kCategoryTypeCellHeight = 63.0f;
 }
 
 - (IBAction)deleteCategory:(id)sender {
+}
+
+- (void)switchBlur
+{
+    if (self.isSelectingTimeType) {
+        [self.view addSubview:self.blurView];
+    } else {
+        [self.blurView removeFromSuperview];
+    }
 }
 
 #pragma mark - Table View methods
