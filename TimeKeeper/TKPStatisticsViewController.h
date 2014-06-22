b603c6da93a1d3619459b82dece65cce9efe3ce1
Maria Saveleva <mariiasaveleva@gmail.com>
2014-06-22 15:16:12 +0400

    Added pieChart view

--- TimeKeeper/TKPStatisticsViewController.h
+++ TimeKeeper/TKPStatisticsViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "XYPieChart.h"
 
 @interface TKPStatisticsViewController : UIViewController
+<
+UITableViewDataSource,
+UITableViewDelegate,
+XYPieChartDataSource,
+XYPieChartDelegate
+>
 
 @end
--- TimeKeeper/TKPStatisticsViewController.m
+++ TimeKeeper/TKPStatisticsViewController.m
@@ -7,13 +7,14 @@
 //
 
 #import "TKPStatisticsViewController.h"
-#import "XYPieChart.h"
 #import "TKPHeaderView.h"
 #import "UIColor+CustomColors.h"
 
 @interface TKPStatisticsViewController ()
 
 @property (weak, nonatomic) IBOutlet TKPHeaderView *headerView;
+@property (weak, nonatomic) IBOutlet XYPieChart *pieChart;
+
 
 @end
 
@@ -34,12 +35,45 @@
     
     [self.headerView enableStatisticMode];
     self.view.backgroundColor = [UIColor categoryCellBackgroundColor];
+    
+    //configure pieChart
+    self.pieChart.delegate = self;
+    self.pieChart.dataSource = self;
+    [self.pieChart setShowPercentage:NO];
+    
+    [self.pieChart setStartPieAngle:M_PI_2];	//optional
+    [self.pieChart setAnimationSpeed:1.0];	//optional
+    [self.pieChart setLabelFont:[UIFont fontWithName:@"DBLCDTempBlack" size:24]];	//optional
+    [self.pieChart setLabelColor:[UIColor blackColor]];	//optional, defaults to white
+    [self.pieChart setLabelShadowColor:[UIColor blackColor]];	//optional, defaults to none (nil)
+    [self.pieChart setLabelRadius:160];	//optional
+    [self.pieChart setShowPercentage:YES];	//optional
+    [self.pieChart setPieBackgroundColor:[UIColor greenColor]];	//optional
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - PieChart delegete & data source methods
+
+- (NSUInteger)numberOfSlicesInPieChart:(XYPieChart *)pieChart
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return 2;
+}
+
+- (CGFloat)pieChart:(XYPieChart *)pieChart valueForSliceAtIndex:(NSUInteger)index
+{
+    CGFloat value = 0.0f;
+    switch (index) {
+        case 0:
+            value = 80.0f;
+            break;
+        case 1:
+            value = 20.0f;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return value;
 }
 
 @end
