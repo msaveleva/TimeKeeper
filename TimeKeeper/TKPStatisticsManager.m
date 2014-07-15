bc811e85a7921718e16f40b454287f1cbf8023d1
Maria Saveleva <mariiasaveleva@gmail.com>
2014-07-15 13:11:39 +0400

    Added current category exception

--- TimeKeeper/TKPStatisticsManager.m
+++ TimeKeeper/TKPStatisticsManager.m
@@ -9,6 +9,10 @@
 #import "TKPStatisticsManager.h"
 #import "TKPCategoryManager.h"
 
+@interface TKPStatisticsManager ()
+
+@end
+
 @implementation TKPStatisticsManager
 
 + (instancetype)sharedInstance
@@ -24,11 +28,32 @@
 
 - (NSArray *)loadCategoriesForType:(TKPCategoryType)type
 {
-    NSArray *categories = [[TKPCategoryManager sharedInstance] loadCategories];
+    NSMutableArray *categories = [NSMutableArray arrayWithArray:[[TKPCategoryManager sharedInstance] loadCategories]];
+    NSString *currentCategoryName = [[TKPCategoryManager sharedInstance] currentCategoryName];
+    for (TKPCategory *category in categories) {
+        if ([category.name isEqualToString:currentCategoryName]) {
+            [categories removeObject:category];
+            break;
+        }
+    }
+    
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"type = %@", @(type)];
     NSArray *toReturn =  [categories filteredArrayUsingPredicate:predicate];
     
     return toReturn;
+}
+
+- (double)loadTimeForType:(TKPCategoryType)type
+{
+    double timeRange = 0;
+    NSArray *categoriesWithType = [self loadCategoriesForType:type];
+    for (TKPCategory *category in categoriesWithType) {
+        for (TKPTimeAndDate *timeAndDate in category.timesAndDates) {
+            //TODO: handle dates
+        }
+    }
+    
+    return timeRange;
 }
 
 @end
