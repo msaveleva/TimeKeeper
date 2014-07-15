d97896d80d681da7c3e240eb8ef427f454b77cf2
Maria Saveleva <mariiasaveleva@gmail.com>
2014-07-15 09:55:00 +0400

    Added gestures for scroll/unscroll

--- TimeKeeper/TKPStatisticsTableHeaderView.m
+++ TimeKeeper/TKPStatisticsTableHeaderView.m
@@ -16,6 +16,14 @@
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *persentLabel;
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+/**
+ Indicates if content of section under this headerView should be visible.
+ Default is NO
+ */
+@property (nonatomic) BOOL isUnscroll;
+
+//gestures
+@property (strong, nonatomic) UITapGestureRecognizer *tapGesture;
 
 @end
 
@@ -29,7 +37,7 @@
         [self addSubview:[[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([TKPStatisticsTableHeaderView class]) owner:self options:nil] lastObject]];
     }
     
-    self.contentView.backgroundColor = [UIColor categoryCellBackgroundColor];
+    [self initialize];
     return self;
 }
 
@@ -41,9 +49,24 @@
         [self addSubview:[[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([TKPStatisticsTableHeaderView class]) owner:self options:nil] lastObject]];
     }
     
-    self.contentView.backgroundColor = [UIColor categoryCellBackgroundColor];
+    [self initialize];
     return self;
 }
 
+- (void)initialize
+{
+    self.contentView.backgroundColor = [UIColor categoryCellBackgroundColor];
+    self.isUnscroll = NO;
+    
+    self.tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTap:)];
+    [self.contentView addGestureRecognizer:self.tapGesture];
+}
+
+#pragma mark - Gestures
+
+- (void)onTap:(id)sender
+{
+    self.isUnscroll = !self.isUnscroll;
+}
 
 @end
